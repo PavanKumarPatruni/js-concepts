6d29bca907a72ab09f41ed9d4fa69cef93ab4386
PavanKumarPatruni <pavanpathro@gmail.com>
2023-12-15 17:01:50 +0530

    feat: webpack integration

--- increment.js
+++ increment.js
@@ -1,3 +1,3 @@
 // increment.js
 
-const incrementByOne = (value) => value + 1;
+export const incrementByOne = (value) => value + 1;
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // index.js
 
+import { incrementByOne } from "./increment";
+
 var count = 0;
 
 function increment() {
   count = incrementByOne(count);
   document.getElementById("count").textContent = count;
 }
+
+document.getElementById("counter").addEventListener("click", increment);
